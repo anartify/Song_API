3532fc1f5ca9fcecc4fab4f73bff16ec1dea0db5
anartify <rajat_j@ee.iitr.ac.in>
2023-05-23 18:05:15 +0530

    Added Testing function for Validation Rules

---
+++ Test/Validation_test.go
@@ -0,0 +1,45 @@
+package Test
+
+import (
+	"Song_API/Models"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSongValidation(t *testing.T) {
+	assert := assert.New(t)
+	song := Models.Song{
+		Song:        "My Song",
+		Artist:      "My Artist",
+		Plays:       10,
+		ReleaseDate: "2022-01-01",
+	}
+
+	song.Song = ""
+	err := song.Validation()
+	assert.Error(err, "validation should fail for empty Song")
+
+	song.Song = "My Song"
+	song.Artist = ""
+	err = song.Validation()
+	assert.Error(err, "validation should fail for empty Artist")
+
+	song.Artist = "My Artist"
+	song.Plays = 0
+	err = song.Validation()
+	assert.Error(err, "validation should fail for empty Plays")
+
+	song.Plays = 10
+	song.ReleaseDate = ""
+	err = song.Validation()
+	assert.Error(err, "validation should fail for empty ReleaseDate")
+
+	song.ReleaseDate = "2022-01-01 00:00:00"
+	err = song.Validation()
+	assert.Error(err, "validation should fail for invalid ReleaseDate")
+
+	song.ReleaseDate = "2022-01-01"
+	err = song.Validation()
+	assert.NoError(err, "validation should pass")
+}
